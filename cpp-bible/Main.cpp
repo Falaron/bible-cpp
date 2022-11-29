75953a21616f169bfb7122e6ed62091ce5284ae6
Arthur <45017124+Falaron@users.noreply.github.com>
2022-11-29 11:16:34 +0100

    adding vectors and tables

--- cpp-bible/Main.cpp
+++ cpp-bible/Main.cpp
@@ -2,16 +2,47 @@
 
 
 void Vector(int vectorSize) {
-    const vector<float>constVector;
-    vector<int>vectorName (vectorSize); /* ALL VECTOR VALUES ARE EQUAL TO 0 */
-
-    vectorName[2] = 5;
-
-    for (int i = 0; i < vectorSize; i++) {
-        if (vectorName[i] == 0) vectorName[i]++; else vectorName[0]--;
-        cout << vectorName[i] << endl;
+    const vector<float>constVector;                     // empty vector && const vector
+    vector<int> vectorOne(vectorSize);                  // n VALUES are equal to 0
+    vector<string> vectorTwo(vectorSize, "hello");      // n VALUES are equal to 0.5
+    vector<int> vectorThree{2, 2, 1, 5};           // vector with specific values
+    
+    cout << "vectorName = ";
+    vectorOne[2] = 5;
+    for (int i = 0; i < vectorOne.size(); i++) {
+        if (vectorOne[i] == 0) vectorOne[i]++; else vectorOne[0]--;
+        cout << vectorOne[i] << " ";
     }
-    // shall print 1 1 5 1 1
+
+
+    cout << "\n\n" << "vectorOne = ";
+    for (string i : vectorTwo) cout << i << " ";
+
+
+    cout << "\n\n" << "vectorThree = ";
+    for (auto &i : vectorThree) cout << i << " ";
+
+
+    cout << "\n\n" << "vectorThree = ";
+    vectorThree = vectorOne;
+    for (auto &i : vectorThree) cout << i << " ";
+
+
+    cout << "\n\n";
+}
+
+void Tables()
+{
+    float table[10];
+    int tableNotDeclared[] = { 12, 5 };     // Table contains 2 elements
+    float notes[4] = {5, 3, 2.5};        // Table contains 4 elements (only 3 declared)
+    int tableTwoDimensions[5][3];        // Table two dimensions
+
+    for (float note : notes)cout << note << " ";
+
+    for (auto & col : tableTwoDimensions)
+        for (auto i : col)
+        { /*element e */ }
 }
 
 int main()
@@ -20,5 +51,6 @@ int main()
     Second second;
     Initialisation init(5,5);
 
-    Vector(5);
+    //Vector(5);
+    Tables();
 }
